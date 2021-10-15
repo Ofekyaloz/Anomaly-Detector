5e0874ec97528f6ae80687501e0fabbbcdce7276
gili gutfeld <giligutfeld@gmail.com>
2021-10-15 17:35:31 +0300

    Upgrade var function

--- cmake-build-debug/anomaly_detection_util.cpp
+++ cmake-build-debug/anomaly_detection_util.cpp
@@ -5,21 +5,16 @@
 #include "anomaly_detection_util.h"
 
 float var(float* x, int size) {
-    float miu = 0, temp, sum = 0;
-    for(int i = 0; i<size; i++){
-        miu += x[i];
-    }
-    miu /= size;
+    float miu = 0, sum = 0;
 
     for(int i = 0; i < size; i++){
-        temp = x[i] - miu;
-        temp *= temp;
-        sum += temp;
+        miu += x[i];
+        sum += x[i] * x[i];
     }
-
+    miu /= size;
     sum /= size;
 
-    return sum;
+    return sum - miu * miu;
 }
 
 float cov(float* x, float* y, int size){
