2a45b41e2801fe250e5be2f1000f572cfbb1e6d2
Ofek <ofek16789@walla.com>
2021-10-15 17:20:50 +0300

    nothing

--- main.cpp
+++ main.cpp
@@ -3,6 +3,5 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, Ofek Ya gever!" << std::endl;
-    std::cout << "supppppppppppppp" << std::endl;
     return 0;
 }
