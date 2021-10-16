e2f1ff1bac90c382b306b3180ff748aeea33b320
gili gutfeld <giligutfeld@gmail.com>
2021-10-16 17:02:18 +0300

    Added dev function, finished part 1

--- cmake-build-debug/anomaly_detection_util.cpp
+++ cmake-build-debug/anomaly_detection_util.cpp
@@ -1,17 +1,16 @@
 //
-// Created by gili on 15/10/2021.
+// Created by Gili and Ofek on 15/10/2021.
 //
 
 #include "anomaly_detection_util.h"
 #include <math.h>
 
 float var(float* x, int size) {
-    float sum = 0;
-
+    float sum = 0, miu = avg(x, size);
     for(int i = 0; i < size; i++){
         sum += x[i] * x[i];
     }
-    float miu = avg(x, size);
+
     return sum / size - miu * miu;
 }
 
@@ -47,8 +46,12 @@ Line linear_reg(Point** points, int size){
     float a = cov(x, y, size) / var(x, size);
     float b = avg(y, size) - a * avg(x, size);
     return Line(a, b);
-
 }
 
+float dev(Point p,Point** points, int size) {
+    return dev(p, linear_reg(points, size));
+}
 
-
+float dev(Point p,Line l) {
+    return abs((p.y - l.f(p.x)));
+}
