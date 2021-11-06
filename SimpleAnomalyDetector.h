caf3b6fbbf5077918d9aa261f3b2e3bf4223f66b
Ofek <ofek.yaloz@live.biu.ac.il>
2021-11-06 20:04:18 +0200

    update files

--- SimpleAnomalyDetector.h
+++ SimpleAnomalyDetector.h
@@ -1,62 +1,36 @@
-#include <string.h>
-#include <vector>
-#include "AnomalyDetector.h"
+#ifndef SIMPLEANOMALYDETECTOR_H_
+#define SIMPLEANOMALYDETECTOR_H_
+
 #include "anomaly_detection_util.h"
-#include "timeseries.h"
+#include "AnomalyDetector.h"
+#include <vector>
+#include <algorithm>
+#include <string.h>
+#include <math.h>
 
 struct correlatedFeatures{
-    string feature1,feature2; // names of the correlated features
+    string feature1,feature2;  // names of the correlated features
     float corrlation;
     Line lin_reg;
     float threshold;
 };
 
-class SimpleAnomalyDetector:public TimeSeriesAnomalyDetector{
-    std::vector<correlatedFeatures> correlated;
-    std::vector<float> distance;
 
+class SimpleAnomalyDetector:public TimeSeriesAnomalyDetector{
+    vector<correlatedFeatures> cf;
 public:
     SimpleAnomalyDetector();
     virtual ~SimpleAnomalyDetector();
 
-    virtual void learnNormal(const TimeSeries& ts) {
-        int i,j, colSize = ts.getColSize();
-        float treshold = 0.9;
-        for (int i = 0; i < colSize; i++) {
-            int m = 0, c = -1;
-            for (int j = i + 1; j < colSize; j++ ) {
-                float* f = &ts.getCol(i)[0];
-                float* g = &ts.getCol(j)[0];
-                float p = pearson(f, g, colSize);
-                if (p > m) {
-                    m = p;
-                    c = j;
-                }
-            }
-            if (c != -1) {
-                Point* ps[colSize];
-                vector<float> v1 = ts.getCol(i), v2 = ts.getCol(j);
-                for (int h = 0; h < colSize; h++)
-                    ps[h]=new Point(v1[h], v2[h]);
-                correlatedFeatures correlatedFeatures(ts.getRowSubject(i), ts.getRowSubject(j), m, lin_reg(ps, colSize), treshold);
-                correlated.push_back(correlateFeatures);
-            }
-        }
-        int corlen = correlated.size();
-        for (i = 0; i < corlen; i++) {
-            vector<float> v1 = ts.getCol(correlated[i].feature1), v2 = ts.getCol(correlated[i].feature2);
-            float max = 0;
-            for (j = 0; j < colSize; j++) {
-                Point p = new Point(v1[j], v2[j]);
-                float dis = dev(p, correlated[i].lin_reg);
-                if (dis > max)
-                    max = dis;
-            }
-            distance.push_back(max);
-        }
+    virtual void learnNormal(const TimeSeries& ts);
+    virtual vector<AnomalyReport> detect(const TimeSeries& ts);
+
+    vector<correlatedFeatures> getNormalModel(){
+        return cf;
     }
 
-    virtual std::vector<AnomalyReport> detect(const TimeSeries& ts);
-
-    std::vector<correlatedFeatures> getNormalModel();
 };
+
+
+
+#endif /* SIMPLEANOMALYDETECTOR_H_ */
--- timeseries.h
+++ timeseries.h
@@ -1,78 +1,19 @@
+
+
 #ifndef TIMESERIES_H_
 #define TIMESERIES_H_
-#include <string.h>
-#include <vector>
-#include <string>
-#include <stdexcept>
-#include <sstream>
-#include <fstream>
-#include <iostream>
 
 using namespace std;
 
-class TimeSeries {
-    vector<pair<string, vector<float>>> table;
+class TimeSeries{
 
 public:
-    // The function reads a CSV file into a vector of pairs of subjects and their vectors
-    TimeSeries(const char* fileName) {
 
-        // Create an input filestream and check the file is open
-        ifstream csv(fileName);
-        if (!csv.is_open()) throw std::runtime_error("Cant open the file");
-
-        string subjects, column, line;
-        float val;
-
-        // Read the subjects of the columns
-        if (csv.good()) {
-            // Get the first line in the file and create a string stream from the line
-            getline(csv, subjects);
-            stringstream ss(subjects);
-
-            // Get each column subject and insert a pair of its name and an empty vector
-            while (getline(ss, column, ',')) {
-                this->table.push_back({column, vector<float>{}});
-            }
-        }
-
-        // Read values from the other lines
-        while (getline(csv, line)) {
-            stringstream ss(line);
-            int count = 0;
-
-            // Get every value in the line
-            while (ss >> val) {
-
-                // Add the current value to the current column's vector
-                table.at(count).second.push_back(val);
-
-                // Check the next token is not a comma, otherwise ignore it and increment the counter
-                if (ss.peek() == ',') ss.ignore();
-                count++;
-            }
-        }
-        csv.close(); // Close the file
-    }
-
-    int getRowSize() const {
-        return this->table.size();
-    }
-
-    int getColSize() const {
-        return this->table[0].second.size();
-    }
-
-    vector<float> getCol(int i) const{
-        vector<float> v = this->table[i].second;
-        return v;
-    }
-
-    string getRowSubject(int i) const {
-        return this->table[i].first;
+    TimeSeries(const char* CSVfileName){
     }
 
 };
+
 
 
 #endif /* TIMESERIES_H_ */
