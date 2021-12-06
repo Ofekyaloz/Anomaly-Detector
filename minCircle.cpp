c25fbebb3180bbd3ea96c49d420066a1bd35c90a
Ofek <ofek.yaloz@live.biu.ac.il>
2021-12-06 16:38:20 +0200

    minCircle

--- minCircle.cpp
+++ minCircle.cpp
@@ -1,13 +1,19 @@
 // C++ program to find the minimum enclosing
 // circle for N integer points in a 2-D plane
+#include <algorithm>
+#include <assert.h>
 #include <iostream>
 #include <math.h>
 #include <vector>
-#include "anomaly_detection_util.h"
 using namespace std;
 
 // Defining infinity
 const double INF = 1e18;
+
+// Structure to represent a 2D point
+struct Point {
+    double X, Y;
+};
 
 // Structure to represent a 2D circle
 struct Circle {
@@ -19,7 +25,8 @@ struct Circle {
 // between two points
 double dist(const Point& a, const Point& b)
 {
-    return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+    return sqrt(pow(a.X - b.X, 2)
+                + pow(a.Y - b.Y, 2));
 }
 
 // Function to check whether a point lies inside
@@ -29,31 +36,31 @@ bool is_inside(const Circle& c, const Point& p)
     return dist(c.C, p) <= c.R;
 }
 
-
-// The following two functions are the functions used
-// To find the equation of the circle when three
-// points are given.
+// The following two functions are used
+// To find the equation of the circle when
+// three points are given.
 
 // Helper method to get a circle defined by 3 points
-Point get_circle_center(float bx, float by,
-                        float cx, float cy)
+Point get_circle_center(double bx, double by,
+                        double cx, double cy)
 {
-    float B = bx * bx + by * by;
-    float C = cx * cx + cy * cy;
-    float D = bx * cy - by * cx;
+    double B = bx * bx + by * by;
+    double C = cx * cx + cy * cy;
+    double D = bx * cy - by * cx;
     return { (cy * B - by * C) / (2 * D),
              (bx * C - cx * B) / (2 * D) };
 }
 
-// Function to return a unique circle that intersects
-// three points
+// Function to return a unique circle that
+// intersects three points
 Circle circle_from(const Point& A, const Point& B,
                    const Point& C)
 {
-    Point I = get_circle_center(B.x - A.x, B.y - A.y,
-                                C.x - A.x, C.y - A.y);
-    I.x += A.x;
-    I.y += A.y;
+    Point I = get_circle_center(B.X - A.X, B.Y - A.Y,
+                                C.X - A.X, C.Y - A.Y);
+
+    I.X += A.X;
+    I.Y += A.Y;
     return { I, dist(I, A) };
 }
 
@@ -62,93 +69,116 @@ Circle circle_from(const Point& A, const Point& B,
 Circle circle_from(const Point& A, const Point& B)
 {
     // Set the center to be the midpoint of A and B
-//    Point C = { (A.x + B.x) / 2.0, (A.y + B.y) / 2.0 };
-    Point* C = new Point((A.x + B.x) / 2.0, (A.y + B.y) / 2.0);
+    Point C = { (A.X + B.X) / 2.0, (A.Y + B.Y) / 2.0 };
 
     // Set the radius to be half the distance AB
-    return { *C, dist(A, B) / 2.0 };
+    return { C, dist(A, B) / 2.0 };
 }
 
-// Function to check whether a circle encloses the given points
-bool is_valid_circle(const Circle& c, const vector<Point>& P)
+// Function to check whether a circle
+// encloses the given points
+bool is_valid_circle(const Circle& c,
+                     const vector<Point>& P)
 {
 
-    // Iterating through all the points to check
-    // whether the points lie inside the circle or not
+    // Iterating through all the points
+    // to check  whether the points
+    // lie inside the circle or not
     for (const Point& p : P)
         if (!is_inside(c, p))
             return false;
     return true;
 }
 
-// Function to return find the minimum enclosing
-// circle from the given set of points
-Circle minimum_enclosing_circle(const vector<Point>& P)
+// Function to return the minimum enclosing
+// circle for N <= 3
+Circle min_circle_trivial(vector<Point>& P)
 {
-
-    // To find the number of points
-    int n = (int)P.size();
-
-    if (n == 0)
+    assert(P.size() <= 3);
+    if (P.empty()) {
         return { { 0, 0 }, 0 };
-    if (n == 1)
+    }
+    else if (P.size() == 1) {
         return { P[0], 0 };
-
-    // Set initial MEC to have infinity radius
-    Circle mec = { { 0, 0 }, INF };
-
-    // Go over all pair of points
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-
-            // Get the smallest circle that
-            // intersects P[i] and P[j]
-            Circle tmp = circle_from(P[i], P[j]);
-
-            // Update MEC if tmp encloses all points
-            // and has a smaller radius
-            if (tmp.R < mec.R && is_valid_circle(tmp, P))
-                mec = tmp;
-        }
+    }
+    else if (P.size() == 2) {
+        return circle_from(P[0], P[1]);
     }
 
-    // Go over all triples of points
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            for (int k = j + 1; k < n; k++) {
+    // To check if MEC can be determined
+    // by 2 points only
+    for (int i = 0; i < 3; i++) {
+        for (int j = i + 1; j < 3; j++) {
 
-                // Get the circle that intersects P[i], P[j], P[k]
-                Circle tmp = circle_from(P[i], P[j], P[k]);
-
-                // Update MEC if tmp encloses all points
-                // and has smaller radius
-                if (tmp.R < mec.R && is_valid_circle(tmp, P))
-                    mec = tmp;
-            }
+            Circle c = circle_from(P[i], P[j]);
+            if (is_valid_circle(c, P))
+                return c;
         }
     }
-    return mec;
+    return circle_from(P[0], P[1], P[2]);
+}
+
+// Returns the MEC using Welzl's algorithm
+// Takes a set of input points P and a set R
+// points on the circle boundary.
+// n represents the number of points in P
+// that are not yet processed.
+Circle welzl_helper(vector<Point>& P,
+                    vector<Point> R, int n)
+{
+    // Base case when all points processed or |R| = 3
+    if (n == 0 || R.size() == 3) {
+        return min_circle_trivial(R);
+    }
+
+    // Pick a random point randomly
+    int idx = rand() % n;
+    Point p = P[idx];
+
+    // Put the picked point at the end of P
+    // since it's more efficient than
+    // deleting from the middle of the vector
+    swap(P[idx], P[n - 1]);
+
+    // Get the MEC circle d from the
+    // set of points P - {p}
+    Circle d = welzl_helper(P, R, n - 1);
+
+    // If d contains p, return d
+    if (is_inside(d, p)) {
+        return d;
+    }
+
+    // Otherwise, must be on the boundary of the MEC
+    R.push_back(p);
+
+    // Return the MEC for P - {p} and R U {p}
+    return welzl_helper(P, R, n - 1);
+}
+
+Circle welzl(const vector<Point>& P)
+{
+    vector<Point> P_copy = P;
+    random_shuffle(P_copy.begin(), P_copy.end());
+    return welzl_helper(P_copy, {}, P_copy.size());
 }
 
 // Driver code
-//int main()
-//{
-//
-//    Circle mec = minimum_enclosing_circle({ { 0, 0 },
-//                                            { 0, 1 },
-//                                            { 1, 0 } });
-//
-//    cout << "Center = { " << mec.C.X << ", " << mec.C.Y
-//         << " } Radius = " << mec.R << endl;
-//
-//    Circle mec2 = minimum_enclosing_circle({ { 5, -2 },
-//                                             { -3, -2 },
-//                                             { -2, 5 },
-//                                             { 1, 6 },
-//                                             { 0, 2 } });
-//
-//    cout << "Center = { " << mec2.C.X << ", " << mec2.C.Y
-//         << " } Radius = " << mec2.R << endl;
-//
-//    return 0;
-//}
+int main()
+{
+    Circle mec = welzl({ { 0, 0 },
+                         { 0, 1 },
+                         { 1, 0 } });
+    cout << "Center = { " << mec.C.X << ", " << mec.C.Y
+         << " } Radius = " << mec.R << endl;
+
+    Circle mec2 = welzl({ { 5, -2 },
+                          { -3, -2 },
+                          { -2, 5 },
+                          { 1, 6 },
+                          { 0, 2 } });
+    cout << "Center = { " << mec2.C.X << ", " << mec2.C.Y
+         << " } Radius = " << mec2.R << endl;
+
+    return 0;
+}
