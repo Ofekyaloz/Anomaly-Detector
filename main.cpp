123d7d1bea1ee7b10d07e15f493fb47b7aaeb678
giligutfeld <80269691+giligutfeld@users.noreply.github.com>
2021-12-08 19:06:37 +0200

    version with aray works faster

--- main.cpp
+++ main.cpp
@@ -1,211 +1,101 @@
+/*
+ * Authors: Gili Gutfeld - 209284512
+ * Ofek Yaloz - 206666729
+ */
 
-#include <iostream>
-#include <vector>
-#include "AnomalyDetector.h"
-#include "SimpleAnomalyDetector.h"
-#include <fstream>
-#include <stdlib.h>     /* srand, rand */
-#include <time.h>       /* time */
 #include "minCircle.h"
 
 using namespace std;
 
-// this is a simple test to put you on the right track
-void generateTrainCSV(float a1,float b1, float a2, float b2){
-    ofstream out("trainFile1.csv");
-    out<<"A,B,C,D"<<endl;
-    Line ac(a1,b1);
-    Line bd(a2,b2);
-    for(int i=1;i<=100;i++){
-        float a=i;
-        float b=rand()%40;
-        out<<a<<","<<b<<","<<ac.f(a)-0.02+(rand()%40)/100.0f<<","<<bd.f(b)-0.02+(rand()%40)/100.0f<<endl;
-    }
-    out.close();
+// return the distance between two points
+float distance(const Point& p1, const Point& p2) {
+    return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
 }
 
-void generateTestCSV(float a1,float b1, float a2, float b2, int anomaly){
-    ofstream out("testFile1.csv");
-    out<<"A,B,C,D"<<endl;
-    Line ac(a1,b1);
-    Line bd(a2,b2);
-    for(int i=1;i<=100;i++){
-        float a=i;
-        float b=rand()%40;
-        if(i!=anomaly)
-            out<<a<<","<<b<<","<<ac.f(a)-0.02+(rand()%40)/100.0f<<","<<bd.f(b)-0.02+(rand()%40)/100.0f<<endl;
-        else
-            out<<a<<","<<b<<","<<ac.f(a)+1<<","<<bd.f(b)-0.02+(rand()%40)/100.0f<<endl;
-    }
-    out.close();
+// check if the circle contains the point
+bool is_in_circle(const Circle& c, const Point& p) {
+    return (distance(c.center, p) <= c.radius);
 }
 
-void checkCorrelationTrain(correlatedFeatures c,string f1, string f2, float a, float b){
-    if(c.feature1==f1){
-        if(c.feature2!=f2)
-            cout<<"wrong correlated feature of "<<f1<<" (-20)"<<endl;
-        else{
-            if(c.corrlation<0.99)
-                cout<<f1<<"-"<<f2<<" wrong correlation detected (-5)"<<endl;
-            if(c.lin_reg.a<a-0.5f || c.lin_reg.a>a+0.5f)
-                cout<<f1<<"-"<<f2<<" wrong value of line_reg.a (-5)"<<endl;
-            if(c.lin_reg.b<b-0.5f || c.lin_reg.b>b+0.5f)
-                cout<<f1<<"-"<<f2<<" wrong value of line_reg.b (-5)"<<endl;
-            if(c.threshold>0.3)
-                cout<<f1<<"-"<<f2<<" wrong threshold detected (-5)"<<endl;
+// get the center of the circle
+Point get_circle_center(float x1, float y1, float x2, float y2) {
+    float a = x1 * x1 + y1 * y1;
+    float b = x2 * x2 + y2 * y2;
+    float c = x1 * y2 - y1 * x2;
+    return Point((y2 * a - y1 * b) / (2 * c), (x1 * b - x2 * a) / (2 * c));
+}
+
+// get the minimum circle from three points by calling the get_circle_center function
+Circle get_circle(const Point& p1, const Point& p2, const Point& p3) {
+    Point I = get_circle_center(p2.x - p1.x, p2.y - p1.y, p3.x - p1.x, p3.y - p1.y);
+    I.x += p1.x;
+    I.y += p1.y;
+    return {I, distance(I, p1)};
+}
+
+// get the minimum circle from two points
+Circle get_circle(const Point& p1, const Point& p2) {
+    Point *p = new Point((p1.x + p2.x) / 2.0, (p1.y + p2.y) / 2.0);
+    return Circle(*p, (distance(p1, p2)) / 2.0);
+}
+
+// check if the circle contains all the points in the vector we got
+bool is_encloses_points(const Circle& c, const vector<Point*>& points) {
+    for (Point *p: points) {
+        if (!is_in_circle(c, *p)){
+            return false;
+        }
+    }
+    return true;
+}
+
+// get the minimum circle from 0, 1, 2 or 3 points
+Circle min_circle_trivial(vector<Point*>& points) {
+    if (points.empty()) {
+        return {{0, 0}, 0};
+    } else if (points.size() == 1) {
+        return {*points[0], 0};
+    } else if (points.size() == 2) {
+        return get_circle(*points[0], *points[1]);
+    }
+
+    // Check if we can find a circle that contains all the three points by only two points
+    for (int i = 0; i < 3; i++) {
+        for (int j = i + 1; j < 3; j++) {
+            Circle c = get_circle(*points[i], *points[j]);
+            if (is_encloses_points(c, points))
+                return c;
         }
     }
 
+    // we must create circle from three points
+    return get_circle(*points[0], *points[1], *points[2]);
 }
 
-
-// last mainTrain
-
-//int main(){
-//    Circle mec = findMinCircle({{0, 0},
-//                        {0, 1},
-//                        {1, 0}});
-//    cout << "Center = { " << mec.center.x << ", " << mec.center.y
-//         << " } Radius = " << mec.radius << endl;
-//
-//    Circle mec2 = findMinCircle({{5,  -2},
-//                         {-3, -2},
-//                         {-2, 5},
-//                         {1,  6},
-//                         {0,  2}});
-//    cout << "Center = { " << mec2.center.x << ", " << mec2.center.y
-//         << " } Radius = " << mec2.radius << endl;
-//
-//    return 0;
-//    srand (time(NULL));
-//    float a1=1+rand()%10, b1=-50+rand()%100;
-//    float a2=1+rand()%20 , b2=-50+rand()%100;
-//
-//
-//    // test the learned model: (40 points)
-//    // expected correlations:
-//    //	A-C: y=a1*x+b1
-//    //	B-D: y=a2*x+b2
-//
-//    generateTrainCSV(a1,b1,a2,b2);
-//    TimeSeries ts("trainFile1.csv");
-//    SimpleAnomalyDetector ad;
-//    ad.learnNormal(ts);
-//    vector<correlatedFeatures> cf=ad.getNormalModel();
-//
-//    if(cf.size()!=2)
-//        cout<<"wrong size of correlated features (-40)" << endl;
-//    else
-//        for_each(cf.begin(),cf.end(),[&a1,&b1,&a2,&b2](correlatedFeatures c){
-//            checkCorrelationTrain(c,"A","C",a1,b1); // 20 points
-//            checkCorrelationTrain(c,"B","D",a2,b2); // 20 points
-//        });
-//
-//    // test the anomaly detector: (60 points)
-//    // one simply anomaly is injected to the data
-//    int anomaly=5+rand()%90; // one anomaly injected in a random time step
-//    generateTestCSV(a1,b1,a2,b2,anomaly);
-//    TimeSeries ts2("testFile1.csv");
-//    vector<AnomalyReport> r = ad.detect(ts2);
-//
-//    bool anomlyDetected=false;
-//    int falseAlarms=0;
-//    for_each(r.begin(),r.end(),[&anomaly,&anomlyDetected,&falseAlarms](AnomalyReport ar){
-//        if(ar.description=="A-C" && ar.timeStep == anomaly)
-//            anomlyDetected=true;
-//        else
-//            falseAlarms++;
-//    });
-//
-//    if(!anomlyDetected)
-//        cout<<"the anomaly was not detected (-30)"<<endl;
-//
-//    if(falseAlarms>0)
-//        cout<<"you have "<<falseAlarms<<" false alarms (-"<<min(30,falseAlarms*3)<<")"<<endl;
-//
-//    cout<<"done"<<endl;
-//    return 0;
-//}
-
-// new MainTrain
-
-#include <iostream>
-#include <vector>
-#include <stdlib.h>
-#include <time.h>
-#include <math.h>
-#include "minCircle.h"
-#include <chrono>
-
-using namespace std;
-using namespace std::chrono;
-
-
-Point** generate(Point center,int R, size_t size){
-    Point** p =new Point*[size];
-    for(size_t i=0;i<size;i++){
-        float r=1+rand()%R;
-        float a=3.14159*(rand()%360)/180;
-        float x=center.x+r*cos(a);
-        float y=center.y+r*sin(a);
-        p[i]=new Point(x,y);
+// welzl algorithm - returns the minimum circle by using in a vector of points and of boundary points.
+// n represents the number of points in points that are not yet processed.
+Circle welzl_algorithm(Point** points, vector<Point*> boundary_points, int n)
+{
+    // Base case when all points processed or |boundary_points| = 3
+    if (n == 0 || boundary_points.size() == 3) {
+        return min_circle_trivial(boundary_points);
     }
-    return p;
+
+    // Get the minimum circle from the vector of points without the last one
+    Circle c = welzl_algorithm(points, boundary_points, n - 1);
+
+    // If c contains the last point, return c
+    if (is_in_circle(c, *points[n - 1])) {
+        return c;
+    }
+
+    // Otherwise, the last point must be on the boundary_points of the minimum circle
+    boundary_points.push_back(points[n - 1]);
+
+    // Return the minimum circle for the vector of points without the last one and boundary_points with the last one
+    return welzl_algorithm(points, boundary_points, n - 1);
 }
 
-
-int main(){
-    srand (time(NULL));
-    const size_t N=250;
-    float R=10+rand()%1000;
-    float cx=-500+rand()%1001;
-    float cy=-500+rand()%1001;
-    Point** ps=generate(Point(cx,cy),R,N);
-
-    // your working copy
-    Point** ps_copy=new Point*[N];
-    for(size_t i=0;i<N;i++)
-        ps_copy[i]=new Point(ps[i]->x,ps[i]->y);
-
-    auto start = high_resolution_clock::now();
-    Circle c=findMinCircle(ps_copy,N);
-    auto stop = high_resolution_clock::now();
-
-    if((int)c.radius>(int)R)
-        cout<<"you need to find a minimal radius (-40)"<<endl;
-
-    bool covered=true;
-    for(size_t i=0;i<N && covered;i++){
-        float x2=(c.center.x-ps[i]->x)*(c.center.x-ps[i]->x);
-        float y2=(c.center.y-ps[i]->y)*(c.center.y-ps[i]->y);
-        float d=sqrt(x2+y2);
-        if(d>c.radius+1)
-            covered=false;
-    }
-    if(!covered)
-        cout<<"all points should be covered (-45)"<<endl;
-
-    auto duration = duration_cast<microseconds>(stop - start);
-    int stime=duration.count();
-    cout<<"your time: "<<stime<<" microseconds"<<endl;
-    if(stime>3000){
-        cout<<"over time limit ";
-        if(stime<=3500)
-            cout<<"(-5)"<<endl;
-        else if(stime<=4000)
-            cout<<"(-8)"<<endl;
-        else if(stime<=6000)
-            cout<<"(-10)"<<endl;
-        else cout<<"(-15)"<<endl;
-    }
-
-    for(size_t i=0;i<N;i++){
-        delete ps[i];
-        delete ps_copy[i];
-    }
-    delete[] ps;
-    delete[] ps_copy;
-
-    cout<<"done"<<endl;
-    return 0;
+Circle findMinCircle(Point** points, size_t size) {
+    return welzl_algorithm(points, {}, size);
 }
